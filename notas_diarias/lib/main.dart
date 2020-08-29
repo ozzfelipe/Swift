62417912fe873aee8661f402bda02460e21cb473
AMcom <felipecardosodossantos@MacBook-Pro-de-Felipe.local>
2020-08-29 15:02:17 -0300

    removido projetos incorretos

--- notas_diarias/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'Home.dart';
-
-void main() => runApp(MaterialApp(
-  home: Home(),
-  debugShowCheckedModeBanner: false,
-));
--- youtube_clone/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'Home.dart';
-
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Home(),
-));
-
