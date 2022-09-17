cf0faf616eeb2274e505720021fc89b7f48c0d2a
Felipe Cardoso dos Santos <felipe.santos@amcom.com.br>
2022-09-16 22:29:56 -0300

    refactor test remoteAddAccount

--- CleanArchApp/DataTests/RemoteAddAccountTests.swift
+++ CleanArchApp/DataTests/RemoteAddAccountTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 final class RemoteAddAccountTests: XCTestCase {
-    func test_() {
+    func test_add_should_call_httpClient_with_correct_url() {
         let url = URL(string: "http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClint: httpClientSpy)
@@ -10,7 +10,7 @@ final class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
-    class HttpClientSpy: HttpClient {
+    class HttpClientSpy: HttpPostClient {
         var url: URL?
 
         func post(url: URL) {
@@ -22,16 +22,16 @@ final class RemoteAddAccountTests: XCTestCase {
 
 }
 
-protocol HttpClient {
+protocol HttpPostClient {
         
     func post(url: URL)
 }
 
 class RemoteAddAccount {
     private let url: URL
-    private let httpClint: HttpClient
+    private let httpClint: HttpPostClient
     
-    init(url: URL, httpClint: HttpClient) {
+    init(url: URL, httpClint: HttpPostClient) {
         self.url = url
         self.httpClint = httpClint
     }
