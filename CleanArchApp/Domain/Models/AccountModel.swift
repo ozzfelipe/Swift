46423d945a7aed0b88e45710726197d9a9425690
Felipe Cardoso dos Santos <felipe.santos@amcom.com.br>
2022-09-16 21:57:51 -0300

    add data layer

---
+++ CleanArchApp/Domain/Models/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Locação on 16/09/22.
+//
+
+import Foundation
+
+struct AccountModel {
+    let name: String
+    let id: String
+    let email: String
+    let password: String
+}
