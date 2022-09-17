91809dbcc58f59f5892621dd0f63891d5ca27ac7
Felipe Cardoso dos Santos <felipe.santos@amcom.com.br>
2022-09-16 21:47:14 -0300

    init app add domain layer

---
+++ CleanArchApp/CleanArchApp/CleanArchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchAppApp.swift
+//  CleanArchApp
+//
+//  Created by Locação on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
